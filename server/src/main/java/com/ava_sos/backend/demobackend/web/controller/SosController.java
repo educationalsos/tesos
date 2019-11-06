83cbdb2227f1e68d30a3c6c20c3aa676ee4abd30
jhonatan <jhonatan_goncalves@id.uff.br>
2019-11-05 21:29:47 -0300

    upload de arquivo na pasta home

--- server/src/main/java/com/ava_sos/backend/demobackend/web/controller/SosController.java
+++ server/src/main/java/com/ava_sos/backend/demobackend/web/controller/SosController.java
@@ -1,5 +1,10 @@
 package com.ava_sos.backend.demobackend.web.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import com.ava_sos.backend.demobackend.domain.Sos;
@@ -13,26 +18,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/sos")
 public class SosController {
-    
-    @Autowired  
-    private SosService sos_service;  
+
+    @Autowired
+    private SosService sos_service;
 
     @PostMapping("/save")
-    @CrossOrigin(origins = "http://localhost:4200")  
-    public void save(@RequestBody Sos sos) {  
-        sos_service.save(sos);  
+    @CrossOrigin(origins = "http://localhost:4200")
+    public void save(@RequestBody Sos sos) {
+        sos_service.save(sos);
 
-    }  
-      
+    }
+
     @GetMapping("/list")
     @CrossOrigin(origins = "http://localhost:4200")
-    public List<Sos> findAll() {  
+    public List<Sos> findAll() {
         return sos_service.findAll();
 
     }
@@ -47,13 +54,23 @@ public class SosController {
     @CrossOrigin(origins = "http://localhost:4200")
     public Sos findByID(@PathVariable("sos_id") long sos_id) {
         return sos_service.findId(sos_id);
-          
-    }  
-      
-    @PostMapping("/update/{sos_id}")  
+
+    }
+
+    @PostMapping("/update/{sos_id}")
     @CrossOrigin(origins = "http://localhost:4200")
-    public void updatesos(@RequestBody Sos sos,@PathVariable("sos_id") long sos_id) {    
-        sos_service.update(sos);  
-    }  
+    public void updatesos(@RequestBody Sos sos, @PathVariable("sos_id") long sos_id) {
+        sos_service.update(sos);
+    }
+
+    @PostMapping("/upload_file")
+    @CrossOrigin(origins = "http://localhost:4200")
+    @ResponseBody
+    public void uploadFile(@RequestPart("file") MultipartFile file) throws IOException {
+        byte[] bytes = file.getBytes();
+        Path path = Paths.get("/home/goncazevedo/", file.getOriginalFilename());
+        Files.write(path, bytes);
+ 
+    }
 
 }
